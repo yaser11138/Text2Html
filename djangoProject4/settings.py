3338639f61add1ce47240610967f96f1a3587ca9
yaser dehghan <70958909+yaser11138@users.noreply.github.com>
2022-04-08 11:22:02 +0430

    Update settings.py

--- djangoProject4/settings.py
+++ djangoProject4/settings.py
@@ -50,7 +50,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "driveyaser@gmail.com"
-EMAIL_HOST_PASSWORD = "y@d@1380"
+EMAIL_HOST_PASSWORD = ""
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
