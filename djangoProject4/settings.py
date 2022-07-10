e49947b64b1c7dcbf98b9ae1deedfc31960d54d2
yaser <ydahgan@gmail.com>
2022-07-10 19:38:05 +0430

    update

--- djangoProject4/settings.py
+++ djangoProject4/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,11 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-CRONJOBS = [
-    ('*/5 * * * *', 'account.cron.my_scheduled_job'),
-
-]
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -137,8 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+
+STATIC_URL = '/static/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
